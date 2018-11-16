0ffcde588430b43447ec8e70e404e2807ec892bb
Bruno Rosell i Gui <rosell@iiia.csic.es>
2018-11-16 09:56:26 +0100

    Replace tabs by spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,32 +2,32 @@
 
 module.exports = function(grunt) {
 
-	var path = require("path");
-	var PWD = process.cwd();
+ var path = require("path");
+ var PWD = process.cwd();
 
-	grunt.initConfig({
-	  jshint : {
-	    options : {
-		    jshintrc : true
-	    },
-	    files : [ "**/amd/src/*.js" ]
-	  },
-	  uglify : {
-		  dynamic_mappings : {
-			  files : grunt.file.expandMapping([ "**/src/*.js", "!**/node_modules/**" ], "", {
-			    cwd : PWD,
-			    rename : function(destBase, destPath) {
-				    destPath = destPath.replace("src", "build");
-				    destPath = destPath.replace(".js", ".min.js");
-				    destPath = path.resolve(PWD, destPath);
-				    return destPath;
-			    }
-			  })
-		  }
-	  },
-	});
+ grunt.initConfig({
+   jshint : {
+     options : {
+      jshintrc : true
+     },
+     files : [ "**/amd/src/*.js" ]
+   },
+   uglify : {
+    dynamic_mappings : {
+     files : grunt.file.expandMapping([ "**/src/*.js", "!**/node_modules/**" ], "", {
+       cwd : PWD,
+       rename : function(destBase, destPath) {
+        destPath = destPath.replace("src", "build");
+        destPath = destPath.replace(".js", ".min.js");
+        destPath = path.resolve(PWD, destPath);
+        return destPath;
+       }
+     })
+    }
+   },
+ });
 
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.registerTask("amd", [ "jshint", "uglify" ]);
+ grunt.loadNpmTasks("grunt-contrib-uglify");
+ grunt.loadNpmTasks("grunt-contrib-jshint");
+ grunt.registerTask("amd", [ "jshint", "uglify" ]);
 };
